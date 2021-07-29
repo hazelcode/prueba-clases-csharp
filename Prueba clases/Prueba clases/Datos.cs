b35d864a040fbf3a75feb57afcd933ba439c4cb6
tacozyt <lacosaqhabla@gmail.com>
2021-07-29 15:22:17 -0500

    Primer codigo fuente

---
+++ Prueba clases/Prueba clases/Datos.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Prueba_clases
+{
+    class Datos
+    {
+        public string Nombre;
+        public int Edad;
+        public string Pais;
+
+        public void MostrarDatos()
+        {
+            string output = "El nombre es {0}, tiene {1} años y vive en {2}";
+            output = string.Format(output, Nombre, Edad, Pais);
+            Console.WriteLine(output);
+        }
+        }
+    }
---
+++ Prueba clases/Prueba clases/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Prueba_clases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Datos DatosPersona = new Datos();
+            DatosPersona.Nombre = "Beto";
+            DatosPersona.Edad = 21;
+            DatosPersona.Pais = "Mexico";
+            DatosPersona.MostrarDatos();
+            Console.Read();
+        }
+    }
+}
